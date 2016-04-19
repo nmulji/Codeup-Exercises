172e7732fb1887bd60a611b29dc758a866bc4d18
Niraj <niraj.mulji@gmail.com>
2016-04-19 11:33:43 -0500

    This is my switch exercise, after I added a switch to show the day of the week.

--- switch.php
+++ switch.php
@@ -9,8 +9,24 @@
 
  switch($dayOfWeek) {
      case 1:
-         // Output Monday
+         echo "Monday\n";
+         break;
      case 2:
-         // Output Tuesday
-     // etc through day 7
+         echo "Tuesday\n";
+         break;
+     case 3:
+         echo "Wednesday\n";
+         break;
+     case 4:
+         echo "Thursday\n";
+         break;        
+     case 5:
+         echo "Friday\n";
+         break;
+     case 6:
+         echo "Saturday\n";
+         break;
+     case 7:
+         echo "Sunday\n";
+         break;           
  }
