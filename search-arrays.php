db370966bfc32d72ff227d3ad6dbdf51f2eb4e7f
Niraj <niraj.mulji@gmail.com>
2016-04-20 15:20:30 -0500

    This is my commit after showing how many values are common in each array.

--- search-arrays.php
+++ search-arrays.php
@@ -8,9 +8,29 @@ $query = 'Tina';
 
 $result = array_search($query, $names);
 
+
 if ($result !== FALSE) {
-	echo "TRUE\n";
+	var_dump(TRUE);
 } else {
-	echo "FALSE\n";
+	var_dump(FALSE);
 }
+
+$count = 0;
+
+foreach ($names as $value) {
+
+	if (array_search($value, $compare) !== false) {
+
+		$count++;
+	}
+}
+
+var_dump($count);
+
+
+
+
+
+
+
 
