3a1c538520160d312f6dbedc24f16eba2229be75
Niraj <niraj.mulji@gmail.com>
2016-04-20 14:09:02 -0500

    This is my commit after searching for 'Tina'.

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,14 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+$query = 'Tina';
+
+$result = array_search($query, $names);
+
+if ($result !== FALSE) {
+	echo "TRUE\n";
+} else {
+	echo "FALSE\n";
+}
+
