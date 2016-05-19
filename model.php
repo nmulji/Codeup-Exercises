0d0721fa670481a3ef9d7384583fb5160a21436e
Niraj <niraj.mulji@gmail.com>
2016-05-19 13:33:03 -0500

    This is my model and user exercise completed.

--- model.php
+++ model.php
@@ -2,29 +2,18 @@
 
 class Model
 {
-	private $attributes = [];
-	private $table;
-
-	// Magic setter to populate $attributes array
-    public function __set($key, $value)
+    protected $attributes;
+    public static $table;
+    
+    public function __set($name, $value)
     {
-        // Set the $name key to hold $value in $data
-        $this->attributes[$key] = $value;
+        $this->attributes[$name] = $value;
     }
-
-   public function __get($key)
+    public function __get($name)
     {
-        // Check for existence of array key $key
-        if (array_key_exists($key, $this->attributes)) {
-            return $this->attributes[$key];
-        }
-
-        return null;
+        return isset($this->attributes[$name]) ? $this->attributes[$name] : null;
     }
-
     public function getTableName()
     {
-    	return $this->table;
+        return static::$table;
     }
-
-}
